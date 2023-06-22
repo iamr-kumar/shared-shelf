57adc4f03314b83cbc3f018c002d1a51a7acafa1
Ritik Kumar <ritik.kumar006@gmail.com>
2023-06-22 17:26:54 +0530

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,14 @@
-import { getAuthenticatedUserFromSession, passage } from '@/utils/passage';
+import { passage } from '@/utils/passage';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
   const authToken = request.cookies.get('psg_auth_token')?.value;
 
   if (!authToken) {
+    return NextResponse.redirect(new URL('/auth', request.url));
+  }
+  const userId = await passage.validAuthToken(authToken);
+  if (!userId) {
     return NextResponse.redirect(new URL('/auth', request.url));
   }
 }
